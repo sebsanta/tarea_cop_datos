d3bd0ab1d590d0883aea1e62144598f4839524ed
sebastian <ssantandersnw@gmail.com>
2023-06-09 02:39:04 -0400

    base datos v2.0

--- crear_basedatos_tabla.sql
+++ crear_basedatos_tabla.sql
@@ -10,3 +10,13 @@ CREATE TABLE `db_users_springboot`.`tarea` (
   `create_at` DATETIME NOT NULL,
   `vigente` TINYINT NOT NULL,
   PRIMARY KEY (`id`));
+
+  --LLENAR LA TABLA TAREAS PARA NO PERDER TIEMPO O 
+  --SIMPLEMENTE POBLAR LA BASE DE DATOS
+  insert into db_tarea_spring.tareas(create_at, descripcion, vigente) values ("2022-11-19","basket",1);
+  insert into db_tarea_spring.tareas(create_at, descripcion, vigente) values ("2023-01-21","video juegos",1);
+  insert into db_tarea_spring.tareas(create_at, descripcion, vigente) values ("2020-10-12","supermecado",0);
+  insert into db_tarea_spring.tareas(create_at, descripcion, vigente) values ("2022-09-14","treking",1);
+  insert into db_tarea_spring.tareas(create_at, descripcion, vigente) values ("2021-11-15","andar en bicicleta",0);
+  insert into db_tarea_spring.tareas(create_at, descripcion, vigente) values ("2022-06-22","ir al gimnasio",1);
+
